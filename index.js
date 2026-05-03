00b3e84465ccb29d52fa152dea1239e236d142e7
Rox Sabil <roxsabil25@gmail.com>
2026-05-04 00:50:58 +0600

    index update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // 1. EJS Setup
 app.set('view engine', 'ejs');
@@ -74,6 +74,8 @@ app.get('/healthz', (req, res) => {
 });
 
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
